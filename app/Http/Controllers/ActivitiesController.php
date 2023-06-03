6250c707ac19bf5d7b057b99a1595a5ce34b97af
Hai Tran <hai@safetyloop.com>
2023-06-03 15:08:35 +0700

    Add dependencies for authorized controllers

--- app/Http/Controllers/ActivitiesController.php
+++ app/Http/Controllers/ActivitiesController.php
@@ -2,6 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+
 
 class ActivitiesController extends Controller
 {
--- app/Http/Controllers/MediaController.php
+++ app/Http/Controllers/MediaController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
 
 class MediaController extends Controller
 {
