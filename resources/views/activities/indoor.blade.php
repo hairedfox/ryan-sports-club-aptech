3ebd0bdd0d843b546b1d87623eddb86c5a5f0fac
HuyCuong411 <cuong.th.2238@aptechlearning.edu.vn>
2023-06-17 14:58:34 +0700

    Outdoor

--- resources/views/activities/indoor.blade.php
+++ resources/views/activities/indoor.blade.php
@@ -3,5 +3,78 @@
 @section('title', 'Indoor Sports')
 
 @section('content')
-  <h1>Indoor Sports</h1>
+      
+      <div class="main container">
+        <div class="wapper">
+            <div class="content1">                                                                
+                <img src="img/Sàn sân bóng rổ bằng gỗ.webp">            
+            </div>       
+        </div>
+        <div class="content2">
+            <div class="row g-5">
+                <div class="col-md-6">
+                  <div class="wrapper">
+                    <div class="block-img">                    
+                      <h5>Project Name</h5>  
+                      <p>This is your Project description. Provide a brief summary to help visitors understand the context and background of your work. Click on "Edit Text" or double click on the text box to start.</p>                  
+                    <img src="img/Nữ leo núi.webp" class="card-img-top" alt="...">                 
+                  </div>
+                  </div>
+                </div>
+                <div class="col-md-6">
+                  <div class="wrapper">
+                    <div class="block-img">                    
+                      <h5>Project Name</h5>  
+                      <p>This is your Project description. Provide a brief summary to help visitors understand the context and background of your work. Click on "Edit Text" or double click on the text box to start.</p>                  
+                    <img src="img/Nữ leo núi.webp" class="card-img-top" alt="...">                 
+                  </div>
+                  </div>
+                </div>
+            </div>
+            <div class="row g-5 mt-1">
+              <div class="col-md-6">
+                <div class="wrapper">
+                  <div class="block-img">                    
+                    <h5>Project Name</h5>  
+                    <p>This is your Project description. Provide a brief summary to help visitors understand the context and background of your work. Click on "Edit Text" or double click on the text box to start.</p>                  
+                  <img src="img/Nữ leo núi.webp" class="card-img-top" alt="...">                 
+                </div>
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="wrapper">
+                  <div class="block-img">                    
+                    <h5>Project Name</h5>  
+                    <p>This is your Project description. Provide a brief summary to help visitors understand the context and background of your work. Click on "Edit Text" or double click on the text box to start.</p>                  
+                  <img src="img/Nữ leo núi.webp" class="card-img-top" alt="...">                 
+                </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="row g-5 mt-1">
+              <div class="col-md-6">
+                <div class="wrapper">
+                  <div class="block-img">                    
+                    <h5>Project Name</h5>  
+                    <p>This is your Project description. Provide a brief summary to help visitors understand the context and background of your work. Click on "Edit Text" or double click on the text box to start.</p>                  
+                  <img src="img/Nữ leo núi.webp" class="card-img-top" alt="...">                 
+                </div>
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="wrapper">
+                  <div class="block-img">                    
+                    <h5>Project Name</h5>  
+                    <p>This is your Project description. Provide a brief summary to help visitors understand the context and background of your work. Click on "Edit Text" or double click on the text box to start.</p>                  
+                  <img src="img/Nữ leo núi.webp" class="card-img-top" alt="...">                 
+                </div>
+                </div>
+              </div>
+            </div>          
+          </div>
+        
+    </div>
+      
+
 @endsection
--- resources/views/activities/outdoor.blade.php
+++ resources/views/activities/outdoor.blade.php
@@ -3,5 +3,96 @@
 @section('title', 'Outdoor Sports')
 
 @section('content')
-  <h1>Outdoor Sports</h1>
+
+    <div class="main">
+        <div class="khoi-tieu-de-outdoor">
+            <div class="elm-wrapper">
+                <div class="elm-chinh-giua">
+                    <div class="outdoor-title">Outdoor</div>
+                <div class="khoi-text-nho" style="padding-left: 450px;padding-right:450px ;">Are you an outdoor lover?
+                    Being outdoors gives you a breath of fresh air and keeps you fit. Let’s explore some of the best activities outdoors. </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="container" style="padding-left:100px;padding-right:100px ;margin-top: 50px;" >
+            <div class="ke-cuoi-dong" style="border-top:3px solid black   ;padding-top: 50px;" >
+                <div class="outdoor-1" style="padding-bottom: 50px;padding-left:70px;padding-right:70px ;">
+                    <div class="row">
+                        <div class="col-5">
+                            <img class="img-full-width" src="img/09d08e3ca4ee47f9b0ace60e440cdee5.webp">
+                        </div>
+                        <div class="col-7">
+                            <div class="elm-wrapper">
+                                <div class="elm-chinh-giua-2" style="display: table-cell;vertical-align: middle;text-align: left;">
+                                    <b>01</b>
+                                    <br>
+                                    <b>Running</b>
+                                    <p>Who doesn’t love a good run? Running is one of the most popular sporting activities in the world. It is a great way to stay active and healthy. Everyone from professional athletes to casual joggers runs regularly as it helps burn extra calories and improve cardiovascular fitness. You can run on the beach, in a forest or somewhere else that you love.</p>       
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="ke-cuoi-dong">
+                <div class="outdoor-2" style="padding-bottom: 50px;padding-left:70px;padding-right:70px ;">
+                    <div class="row">
+                        <div class="col-7">
+                            <div class="elm-wrapper">
+                                <div class="elm-chinh-giua-2" style="display: table-cell;vertical-align: middle;text-align: left;">
+                                    <b>01</b>
+                                    <br>
+                                    <b>Running</b>
+                                    <p>Who doesn’t love a good run? Running is one of the most popular sporting activities in the world. It is a great way to stay active and healthy. Everyone from professional athletes to casual joggers runs regularly as it helps burn extra calories and improve cardiovascular fitness. You can run on the beach, in a forest or somewhere else that you love.</p>       
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-5">
+                            <img class="img-full-width" src="img/09d08e3ca4ee47f9b0ace60e440cdee5.webp">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="ke-cuoi-dong">
+                <div class="outdoor-3" style="padding-bottom: 50px;padding-left:70px;padding-right:70px ;">
+                    <div class="row">
+                        <div class="col-5">
+                            <img class="img-full-width" src="img/09d08e3ca4ee47f9b0ace60e440cdee5.webp">
+                        </div>
+                        <div class="col-7">
+                            <div class="elm-wrapper">
+                                <div class="elm-chinh-giua-2" style="display: table-cell;vertical-align: middle;text-align: left;">
+                                    <b>01</b>
+                                    <br>
+                                    <b>Running</b>
+                                    <p>Who doesn’t love a good run? Running is one of the most popular sporting activities in the world. It is a great way to stay active and healthy. Everyone from professional athletes to casual joggers runs regularly as it helps burn extra calories and improve cardiovascular fitness. You can run on the beach, in a forest or somewhere else that you love.</p>       
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="ke-cuoi-dong">
+                <div class="outdoor-4" style="padding-bottom: 50px;padding-left:70px;padding-right:70px ;" >
+                    <div class="row">
+                        <div class="col-7">
+                            <div class="elm-wrapper">
+                                <div class="elm-chinh-giua-2" style="display: table-cell;vertical-align: middle;text-align: left;">
+                                    <b>01</b>
+                                    <br>
+                                    <b>Running</b>
+                                    <p>Who doesn’t love a good run? Running is one of the most popular sporting activities in the world. It is a great way to stay active and healthy. Everyone from professional athletes to casual joggers runs regularly as it helps burn extra calories and improve cardiovascular fitness. You can run on the beach, in a forest or somewhere else that you love.</p>       
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-5">
+                            <img class="img-full-width" src="img/09d08e3ca4ee47f9b0ace60e440cdee5.webp">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>    
+
 @endsection
