8657fd13b4f6dac04c9e0250e94c0c032aed4665
Hai Tran <hai@safetyloop.com>
2023-07-04 01:40:08 +0700

    Add activity scope

--- app/Models/Activity.php
+++ app/Models/Activity.php
@@ -9,4 +9,16 @@ class Activity extends Model
 {
     use HasFactory;
     protected $fillable = ['name', 'timing', 'rules', 'min_people', 'max_people', 'famous_players', 'creator_id'];
+
+    public static function indoor() {
+        return self::where('type', 'indoor')->orderBy('created_at')->get();
+    }
+
+    public static function outdoor() {
+        return self::where('type', 'outdoor')->orderBy('created_at')->get();
+    }
+
+    public static function recreation() {
+        return self::where('type', 'recreation')->orderBy('created_at')->get();
+    }
 }
